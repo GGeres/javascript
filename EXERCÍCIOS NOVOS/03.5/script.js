397aa768103988fe44f308fa97d91bb7ed3b519c
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-15 17:48:44 -0300

    Novo Treino

---
+++ EXERCÍCIOS NOVOS/03.5/script.js
@@ -0,0 +1,16 @@
+
+let jvelha = document.getElementsByClassName('velha')
+let valores = []
+let psimb = document.getElementsByName('Players')
+let simb = ''
+if(psimb[0].checked){
+    simb = valores[0] = X
+}else{
+    simb = valores[1] = O
+}
+
+
+
+
+
+
--- EXERCÍCIOS NOVOS/03/script.js
+++ EXERCÍCIOS NOVOS/03/script.js
@@ -1,72 +1,78 @@
+let jogo = [X,O]
+let px = document.getElementsByName('Players')
+let po = document.getElementsByName('Players')
+
+let nw = document.getElementById('NW')
+let n = document.getElementById('N')
+let ne = document.getElementById('NE')
+let cw = document.getElementById('CW')
+let c = document.getElementById('C')
+let ce = document.getElementById('CE')
+let sw = document.getElementById('SW')
+let s = document.getElementById('S')
+let se = document.getElementById('SE')
+
 function acenderNW(){
-    let nw = document.getElementById('NW')
     nw.style.background = 'yellow'
 }
 function apagarNW(){
-    let nw = document.getElementById('NW')
     nw.style.background = 'lightgray'
 }
 function acenderN(){
-    let n = document.getElementById('N')
     n.style.background = 'yellow'
 }
 function apagarN(){
-    let n = document.getElementById('N')
     n.style.background = 'lightgray'
 }
 function acenderNE(){
-    let ne = document.getElementById('NE')
     ne.style.background = 'yellow'
 }
 function apagarNE(){
-    let ne = document.getElementById('NE')
     ne.style.background = 'lightgray'
 }
 function acenderCW(){
-    let cw = document.getElementById('CW')
     cw.style.background = 'yellow'
 }
 function apagarCW(){
-    let cw = document.getElementById('CW')
     cw.style.background = 'lightgray'
 }
 function acenderC(){
-    let c = document.getElementById('C')
     c.style.background = 'yellow'
 }
 function apagarC(){
-    let c = document.getElementById('C')
     c.style.background = 'lightgray'
 }
 function acenderCE(){
-    let ce = document.getElementById('CE')
     ce.style.background = 'yellow'
 }
 function apagarCE(){
-    let ce = document.getElementById('CE')
     ce.style.background = 'lightgray'
 }
 function acenderSW(){
-    let sw = document.getElementById('SW')
     sw.style.background = 'yellow'
 }
 function apagarSW(){
-    let sw = document.getElementById('SW')
     sw.style.background = 'lightgray'
 }
 function acenderS(){
-    let s = document.getElementById('S')
     s.style.background = 'yellow'
 }
 function apagarS(){
-    let s = document.getElementById('S')
     s.style.background = 'lightgray'
 }
 function acenderSE(){
-    let se = document.getElementById('SE')
     se.style.background = 'yellow'
 }
 function apagarSE(){
-    let se = document.getElementById('SE')
     se.style.background = 'lightgray'
+}
+
+
+if(px[0].checked){
+    jogo = 'X'
+    nw.innerText = `${jogo}`
+}else{
+    jogo = 'O'
+    nw.innerText = `${jogo}`
+
 }
