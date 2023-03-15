80a08395e3b93a3fc551e7ab92bf18603168421b
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-15 16:31:55 -0300

    Novas Práticas

--- EXERCÍCIOS NOVOS/02/script.js
+++ EXERCÍCIOS NOVOS/02/script.js
@@ -3,3 +3,9 @@ function trocar(){
     l.src = 'lampada-on.jpg'
     console.log(l)
 }
+
+function desligar(){
+    let l2 = document.getElementById('lamp')
+    l2.src = 'lampada.jpg'
+    console.log(l2)
+}
---
+++ EXERCÍCIOS NOVOS/03/script.js
@@ -0,0 +1,72 @@
+function acenderNW(){
+    let nw = document.getElementById('NW')
+    nw.style.background = 'yellow'
+}
+function apagarNW(){
+    let nw = document.getElementById('NW')
+    nw.style.background = 'lightgray'
+}
+function acenderN(){
+    let n = document.getElementById('N')
+    n.style.background = 'yellow'
+}
+function apagarN(){
+    let n = document.getElementById('N')
+    n.style.background = 'lightgray'
+}
+function acenderNE(){
+    let ne = document.getElementById('NE')
+    ne.style.background = 'yellow'
+}
+function apagarNE(){
+    let ne = document.getElementById('NE')
+    ne.style.background = 'lightgray'
+}
+function acenderCW(){
+    let cw = document.getElementById('CW')
+    cw.style.background = 'yellow'
+}
+function apagarCW(){
+    let cw = document.getElementById('CW')
+    cw.style.background = 'lightgray'
+}
+function acenderC(){
+    let c = document.getElementById('C')
+    c.style.background = 'yellow'
+}
+function apagarC(){
+    let c = document.getElementById('C')
+    c.style.background = 'lightgray'
+}
+function acenderCE(){
+    let ce = document.getElementById('CE')
+    ce.style.background = 'yellow'
+}
+function apagarCE(){
+    let ce = document.getElementById('CE')
+    ce.style.background = 'lightgray'
+}
+function acenderSW(){
+    let sw = document.getElementById('SW')
+    sw.style.background = 'yellow'
+}
+function apagarSW(){
+    let sw = document.getElementById('SW')
+    sw.style.background = 'lightgray'
+}
+function acenderS(){
+    let s = document.getElementById('S')
+    s.style.background = 'yellow'
+}
+function apagarS(){
+    let s = document.getElementById('S')
+    s.style.background = 'lightgray'
+}
+function acenderSE(){
+    let se = document.getElementById('SE')
+    se.style.background = 'yellow'
+}
+function apagarSE(){
+    let se = document.getElementById('SE')
+    se.style.background = 'lightgray'
+}
