f0064c5356feae97cc7afcc6b4eeb8ae574173c8
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-27 11:12:52 -0300

    Novas práticas

--- EXERCÍCIOS NOVOS/03.5/script.js
+++ EXERCÍCIOS NOVOS/03.5/script.js
@@ -1,13 +1,37 @@
 
 let jvelha = document.getElementsByClassName('velha')
-let valores = []
-let psimb = document.getElementsByName('Players')
+let valores;
+let player = document.getElementsByName('Players')
 let simb = ''
-if(psimb[0].checked){
-    simb = valores[0] = X
-}else{
-    simb = valores[1] = O
+let X = document.getElementById('PX')
+let O = document.getElementById('PO')
+let positions =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+];
+
+function jog(p){
+    if(p = X){
+        return 'X'
+    } else if(p = O){
+        return 'O'
+    }
 }
+if(player[0]){
+    p = X
+}else{
+    p = O
+}
+
+
+
+
 
 
 
